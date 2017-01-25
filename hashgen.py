6e8811ebd67f9a72032cdc7a3618a9323aa1513f
elusivepeak <ziphound@gmail.com>
2017-01-26 11:47:50 +1300

    updated to use random instead of hash and match latest naming standard

--- hashgen.py
+++ hashgen.py
@@ -1,38 +1,32 @@
-
+# #!/usr/local/bin/python3
+# hashgen.py
+# Dave Grainger / @elusivepeak January 2017
+# Generates sample S3 object keys for dataset names in the S3 in-bucket
+# 
+# /[4 digit hash]/[origin type]-[origin id]/[year]-[month]-[day]-[hour]-[minute]/[dataset name]{.[start]}{-[extent}}
 #
-# dataset origin name is a 4 dotted word name
-#	type.source.schema.collection
+# origin is a dotted hierarchical name describing the datasource
 #
-# Example for a relational source (c.f. DataVault)
-# 	relational.MyXero.dbo.Organisation
-#
-# Example for unstructured sources
-# 	unstructured.sumo.p_users2.accesslogs
-# 	unstructured.datadog.stream7.objectclassX
-#
-# Define "LAKE EPOCH" as 01/01/2017
-# Calculate N the number of days since LAKE EPOCH that the extract dataset was produced
-# Calculate the hash of <originname> + str(N)  e.g. today "MyXero.dbo.Organisation24"
-# Truncate the hash to the right 2 bytes / 16 bits / 4 hex digits HHHH
-# Dataset filename format
-# 	HHHH-NNNN-originsource.schema.table-BBB
-# where HHHH is righthand 16 bits (4 hex digits) of the hash
-# where NNNN is number of days since lake epoch
-# where BBB is the batch number (allows a big dataset to be broken down into batches)
+# Example for a sql origin type (c.f. DataVault)
+# 	MyXero.dbo.Organisation   (database.schema.collection)
 
-# python example for generating name of some relational-origin datasets
+import random    	# for randint() function
+import datetime		# for 'now'
 
-datasetorigins = [	'MyXero.dbo.Organisation', 
-			'MyXero.dbo.OrganisationDescription',
-			'Shard.dbo.Shardmonkey',
-			'Payroll.dbo.Salary',
-			'Payroll.dbo.AntonysSalary']
+# invent some sample sql origin datasources and dataset names
+sqlorigins = {'MyXero.dbo.Organisation': 'SubscribedOrgs', 
+				'MyXero.dbo.OrganisationDescription': 'OrganisationDetail',
+				'Shard.dbo.Shardmonkey': 'OrgsByShard',
+				'Payroll.dbo.Salary':'SalariedEmployees' ,
+				'Payroll.dbo.AntonysSalary':'UnderpaidEmployees' }
 
-# iterate for a sample range of dates 180-200 since epoch
-for i in range(180,200):
-	for j in range(1,5):       		# iterate across five origin dataset names
-		hhhh = hex( hash(datasetorigins[j] + str(i)) &0xFFFF)
-		datasetname = hhhh + "-" + datasetorigins[j] + "-001"
-		print "Days since epoch",i, "	Datasetname", datasetname
-			 
+# generate 10 sample dataset names
+for sample in sqlorigins:
+	origintype='sql'
+	
+	rand = random.randint(1,2**16)		# generate a 16-bit random integer
 
+	et = datetime.datetime.now()		# take now as time of dataset extract
+	edt = str(f'{et.year}-{et.month}-{et.day}-{et.hour}-{et.minute}')
+
+	print(f'/{rand:04x}/sql/{sample}/{edt}/{sqlorigins[sample]}.1-10000')
